c8d6d9e8c5781ea4a79ab72b85a59d09d1f6fbf4
David Baker <dibakergro@btinternet.com>
2024-12-02 15:55:07 +0000

    Add heaviside function so things actually work, apparently make some other changes too

---
+++ code/CommonTools/heaviside.m
@@ -0,0 +1,4 @@
+function y = heaviside(x)
+y = (x>=0);
+end
+
--- code/GenericInversion/Scattering/Distributed_Source/WallPressure_PointSource_FarField_routine.m
+++ code/GenericInversion/Scattering/Distributed_Source/WallPressure_PointSource_FarField_routine.m
@@ -11,14 +11,14 @@ delta_points_sheared = [0,0.3,1];
 U_sheared = Struct_U_PWParabolic(U_points_sheared,delta_points_sheared);
 
 
-y2 = 0.1:0.1:0.5;
+y2 = linspace(0, 1, 40);
 shift_y2 = 0.005;
 
 delta = delta_points_base(end); % should hopefully be same both base and sheared
 Uinf = U_base.f(delta);
 cinf = 35;
 
-omega = logspace(0,3,30);
+omega = logspace(-1,3,40);
 f = 10*omega./(2*pi*0.02);
 
 % Various computational parameters
@@ -36,10 +36,10 @@ for j = 1:numel(omega)
     U = U_base;
     WallPressure_PointSource_FarField;
     FF_Pressure_base(j,:) = FF1p.p(:);
-    PlotRI(y2,FF_Pressure_base(j,:));
+%     PlotRI(y2,FF_Pressure_base(j,:));
     U = U_sheared;
     WallPressure_PointSource_FarField;
     FF_Pressure_sheared(j,:) = FF1p.p(:);
-    PlotRI(y2,FF_Pressure_sheared(j,:));
+%     PlotRI(y2,FF_Pressure_sheared(j,:));
     disp([num2str(j),' of ',num2str(numel(omega))]);
 end
